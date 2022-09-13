71c6cab81840d6a207024be4517abf3598bd9553
Code Sandbox <hello@codesandbox.io>
2022-09-13 07:52:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.73363f44bdce46c68f115e26006cda7d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f9109b97871a7a8ce36d1c337f83d1",
+    "url": "https://mehnaaztabasum.github.io/csb-55pgq9/index.html"
+  },
+  {
+    "revision": "a0d125f3b56b14f24325",
+    "url": "https://mehnaaztabasum.github.io/csb-55pgq9/static/js/2.e5e5a5f0.chunk.js"
+  },
+  {
+    "revision": "609e1560a041fe56dd50",
+    "url": "https://mehnaaztabasum.github.io/csb-55pgq9/static/js/main.43a60c8b.chunk.js"
+  },
+  {
+    "revision": "9c825d2afbc2146cf990",
+    "url": "https://mehnaaztabasum.github.io/csb-55pgq9/static/js/runtime-main.253524f5.js"
+  }
+]);
